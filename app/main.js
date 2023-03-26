3ec897e18f9774a4f3669337cd868574862c15ef
ashrafatef <ashraaf.ateef@gmail.com>
2023-03-27 01:53:58 +0200

    handle multiple ping

--- app/main.js
+++ app/main.js
@@ -5,29 +5,21 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
     connection.on('data', (data)=>{
-        // convert from buffer to string
         const cleanCommands = formatCommand(data)
-        console.log('cleaned commands' , cleanCommands)
-        let results;
-        switch (cleanCommands[0].toLowerCase()){
-            case 'ping':
+        switch (cleanCommands[0]){
+            case 'PING':
                 connection.write('+PONG\r\n')
                 break
             default:
                 connection.write('+OK\r\n')
         }
-        // console.log(results)
-        // connection.write('+PONG\r\n')
-        // return 'PONG'
     })
 });
 
 const formatCommand = (buf) =>{
     const convertedBinaryToString = Buffer.from(buf).toString()
-    return convertedBinaryToString.replace(/(\*[0-9])|(\$[0-9])/g, ' ').trim().split(' ').map(str => str.trim())
-
+    return convertedBinaryToString.replace( /(\n)|(\r)|(\*[0-9])|(\$[0-9])/g, ' ').split(' ').filter(st => !!st)
 }
 
 server.listen(6379, "127.0.0.1");
