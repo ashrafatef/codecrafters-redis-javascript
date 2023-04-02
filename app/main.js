6c9d590181d574f6c708504e6d8f2b68694ae95b
ashrafatef <ashraaf.ateef@gmail.com>
2023-04-02 12:15:06 +0200

    add echo command

--- app/main.js
+++ app/main.js
@@ -5,14 +5,31 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
     connection.on('data', (data)=>{
-        connection.write('+PONG\r\n')
+        let results;
+        const command = formatCommand(data)
+        console.log(command)
+        if (command[0].toLowerCase() === 'ping') {
+            results = '+PONG'
+        } else if (command[0] === 'ECHO') {
+            command.shift()
+            results = handleEchoCommand(command)
+        } else {
+            connection.write(`-Error message : unkown command  ${command[0]}\r\n`)
+        }
+        return connection.write(`${results}\r\n`)
     })
 });
 
-const formatCommand = (buf) =>{
+const formatCommand = (buf) => {
     const convertedBinaryToString = Buffer.from(buf).toString()
-    console.log(convertedBinaryToString)
-    return convertedBinaryToString.replace( /(\n)|(\r)|(\*[0-9])|(\$[0-9])/g, ' ').split(' ').filter(st => !!st)
+    return convertedBinaryToString.replace(/(\n)|(\r)|(\*[0-9])|(\$[0-9])/g, ' ').split(' ').filter(st => !!st)
+}
+
+const handleEchoCommand = (arguments) => {
+    if (arguments.length === 1) {
+        return `+${arguments[0]}`
+    }
+    return '- Error Message invalid arguments for ECHO Command'
 }
 
 server.listen(6379, "127.0.0.1");
